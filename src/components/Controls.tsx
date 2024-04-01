b26334ec433aef82d27103300092c8179f298fc7
Guillem Orellana Trullols <guillem.orellana@gmail.com>
2024-04-01 13:02:25 +0200

    improve controls

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,5 +1,5 @@
 import { useBoundStore } from "@/store"
-import { RiMoonLine, RiSettingsLine, RiSunLine } from "@remixicon/react"
+import { RiDashboard3Line, RiMoonLine, RiSettingsLine, RiSunLine } from "@remixicon/react"
 import { Button } from "@tremor/react"
 
 export default function Control() {
@@ -10,18 +10,32 @@ export default function Control() {
     state.toggleDarkMode,
   ])
 
-  const isSettingsDisabled = inSettingsScreen == null
-
   return (
     <div className="flex flex-row justify-end w-full p-4 gap-x-4">
+      {inSettingsScreen ? (
+        <Button
+          variant="light"
+          tooltip="Back to dashboard"
+          size="lg"
+          icon={RiDashboard3Line}
+          onClick={() => setInSettingsScreen(false)}
+        />
+      ) : null}
       <Button
         variant="light"
-        disabled={isSettingsDisabled}
+        disabled={inSettingsScreen}
         size="lg"
+        tooltip="Open settings screen"
         icon={RiSettingsLine}
         onClick={() => setInSettingsScreen(true)}
       />
-      <Button variant="light" size="lg" icon={darkMode ? RiSunLine : RiMoonLine} onClick={toggleDarkMode} />
+      <Button
+        variant="light"
+        tooltip="Toggle dark mode"
+        size="lg"
+        icon={darkMode ? RiSunLine : RiMoonLine}
+        onClick={toggleDarkMode}
+      />
     </div>
   )
 }
