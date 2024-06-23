75a5b86d2f2dac99f15a5093b97839d05d37baa2
Guillem96 <guillem.orellana@gmail.com>
2024-06-23 19:53:48 +0200

    hotfix: remove asset by risk and tag

--- src/features/assets/kpis/index.ts
+++ src/features/assets/kpis/index.ts
@@ -1,6 +1,4 @@
 export { default as AssetBarList } from "./AssetBarList"
-// export { default as RiskDonut } from "./RiskDonut"
-// export { default as TagDonut } from "./TagDonut"
 export { default as CountryDonut } from "./CountryDonut"
 export { default as SectorDonut } from "./SectorDonut"
 export { default as TotalAssetValue } from "./TotalAssetValue"
